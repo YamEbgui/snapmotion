2044531722a49e8fe04eb56e1459a3816f843db9
Yam Ebgui <yam.ebgui@gmail.com>
2025-07-17 19:44:50 +0300

    change record frame libary

--- src/app/api/services/framesExtraction.ts
+++ src/app/api/services/framesExtraction.ts
@@ -1,30 +1,31 @@
-import { spawn } from 'child_process'
 import { randomUUID } from "crypto";
-import { execa } from 'execa';
-import { tmpdir } from 'os';
-import { join as pathJoin } from 'path';
+import { tmpdir } from "os";
+import { join as pathJoin } from "path";
+import ffmpeg from "fluent-ffmpeg";
 
 const tmpDir = tmpdir();
-const TAKE_AT_SECONDS = '4'
-const FILE_EXTENSION = '.png'
+const TAKE_AT_SECONDS = "3";
+const FILE_EXTENSION = ".png";
 
 export async function extractFrame(videoUrl: string): Promise<string> {
-    try {
+    return new Promise((resolve, reject) => {
         const frameId = randomUUID();
         const fileName = frameId + FILE_EXTENSION;
         const videoPath = pathJoin(tmpDir, videoUrl);
-        const outputPath = pathJoin(tmpDir, fileName);
 
-        await execa('ffmpeg', [
-            '-i', videoPath,
-            '-ss', TAKE_AT_SECONDS,
-            '-vframes', '1',
-            outputPath
-        ]);
-
-        return fileName;
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+        ffmpeg(videoPath)
+            .screenshots({
+                timestamps: [TAKE_AT_SECONDS],
+                filename: fileName,
+                folder: tmpDir,
+                size: '640x?'
+            })
+            .on('end', () => {
+                resolve(fileName);
+            })
+            .on('error', (err) => {
+                console.log("error while using ffmpeg", err);
+                reject(err);
+            });
+    });
 }
