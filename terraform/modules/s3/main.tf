8a642b7b50ba82bdc1676d77bd946e79d44e53a2
Yam Ebgui <yam.ebgui@gmail.com>
2025-07-19 12:18:14 +0300

    adding cors configuration for s3 to enable download frames

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -19,3 +19,22 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# CORS configuration for the S3 bucket(to enable download)
+resource "aws_s3_bucket_cors_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = [
+      "http://localhost:3000",
+      "http://localhost:3001", 
+      "https://*.amazonaws.com",
+      "https://snapmotion.com",
+      "https://*.snapmotion.com"
+    ]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
